04b6f9ef055a81c0bca7f402b93a2fa3141c1ebc
jaecea <jaroslava.comova@gmail.com>
2024-04-29 13:21:26 +0200

    added last working installation files

---
+++ install/preinst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install qt6-base-dev
+sudo apt --fix-broken install
+sudo apt-get install qt6-base-dev
+sudo dpkg -i calculator-1.0-Alpha-Linux.deb
